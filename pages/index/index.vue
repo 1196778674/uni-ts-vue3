0dda2967c8d65337db9927e2dfe4e5aeaaed450f
刘飞 <liufei@bizseer.com>
2021-09-28 14:41:54 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>{{state.title}}</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent, reactive, toRefs } from 'vue';
+	export interface IState {
+		title: string
+	}
+	export default defineComponent({
+		name: 'Index',
+		setup(){
+			const state = reactive<IState>({
+				title: 'hello'
+			})
+			
+			return {
+				state
+			}
+		}
+	})
+</script>
+
+<style>
+	
+</style>
